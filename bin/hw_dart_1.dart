a8d599fac39144d4a0ce9dad9881b1a1ca494b6e
RuslanPalamarchuk <85477062+RuslanPalamarchuk@users.noreply.github.com>
2023-05-02 15:59:55 +0300

    first commit

---
+++ bin/hw_dart_1.dart
@@ -0,0 +1,56 @@
+import 'dart:convert' as convert;
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:sqlite3/sqlite3.dart';
+
+void main(List<String> arguments) async {
+  final db = sqlite3.open('Names.db');
+
+  final checkTable = db.select(
+      'SELECT name FROM sqlite_master WHERE type="table" AND name="Names";');
+  if (checkTable.isEmpty) {
+    db.execute('''
+    CREATE TABLE Names (
+      count INTEGER NOT NULL,
+      gender TEXT NOT NULL,
+      name TEXT NOT NULL PRIMARY KEY UNIQUE,
+      probability REAL
+    );
+  ''');
+  }
+
+  final name = 'lena';
+  final url = Uri.https('api.genderize.io', '', {'name': name});
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    final jsonResponse = convert.jsonDecode(response.body);
+    final count = jsonResponse['count'];
+    final gender = jsonResponse['gender'];
+    final probability = jsonResponse['probability']?.toDouble();
+    final stmt = db.prepare(
+        'INSERT OR REPLACE INTO Names (count, gender, name, probability) VALUES (?, ?, ?, ?)');
+    stmt.execute([count, gender, name, probability]);
+    stmt.dispose();
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+  }
+
+  final resultSet = db.select('SELECT * FROM Names');
+  for (final row in resultSet) {
+    print(
+        'count: ${row['count']}, gender: ${row['gender']}, name: ${row['name']}, probability: ${row['probability']}');
+  }
+
+  final file = File('Names.txt');
+  final output = file.openWrite();
+  for (final row in resultSet) {
+    final line =
+        '${row['count']}\t${row['gender']}\t${row['name']}\t${row['probability']}';
+    output.write('$line\r\n');
+  }
+  await output.close();
+
+  db.dispose();
+}
---
+++ test/hw_dart_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:hw_dart_1/hw_dart_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
